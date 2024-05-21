19186fb870229e4ec2cf4b53250651186f28f437
camilo <camilolavado@petalmail.com>
2024-05-20 20:16:28 -0400

    efecto de maquina de escribir

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -1,11 +1,39 @@
 // src/pages/About.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Typewriter from 'typewriter-effect';
+import { useInView } from 'react-intersection-observer';
+import './styles/About.css';
 
 const About = () => {
+    const { ref, inView } = useInView({
+        triggerOnce: true, // Solo se activa una vez
+        threshold: 0.1, // Se activa cuando el 10% del elemento está en vista
+    });
+    const [startTyping, setStartTyping] = useState(false);
+
+    useEffect(() => {
+        if (inView) {
+            setStartTyping(true);
+        }
+    }, [inView]);
+
     return (
-        <section id="about" className="about-section">
+        <section id="about" className="about-section" ref={ref}>
             <h2>Sobre Mí</h2>
-            <p>Desarrollador Full Stack JavaScript y Java, con formación en Bootcamps y diplomado en gestión ágil de equipos. Profesional enfocado en el trabajo en equipo, el aprendizaje continuo y el cumplimiento de objetivos.</p>
+            {startTyping && (
+                <Typewriter
+                    options={{
+                        strings: [
+                            "Desarrollador Full Stack JavaScript y Java, con formación en Bootcamps y diplomado en gestión ágil de equipos.",
+                            "Profesional enfocado en el trabajo en equipo, el aprendizaje continuo y el cumplimiento de objetivos."
+                        ],
+                        autoStart: true,
+                        loop: true,
+                        delay: 70,
+                        deleteSpeed: 20,
+                    }}
+                />
+            )}
         </section>
     );
 };
