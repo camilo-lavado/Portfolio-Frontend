27b045bdda300d9e4843de4b2886d581912885c5
camilo <camilolavado@petalmail.com>
2024-05-20 20:37:42 -0400

    Efecto Hover

--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Carousel } from 'primereact/carousel';
 import ProjectCard from '../components/ProjectCard';
 import './styles/Projects.css';
-
 import ADDataImage from '../assets/media/AD-data.png';
 import ADigitalesImage from '../assets/media/Adigitales.png';
 
@@ -59,6 +58,10 @@ const Projects = () => {
 
     return (
         <div className="projects">
+            <div className="projects-header">
+                <i className="pi pi-fw pi-briefcase projects-icon"></i>
+                <h1>Proyectos</h1>
+            </div>
             <Carousel
                 value={projects}
                 itemTemplate={projectTemplate}
